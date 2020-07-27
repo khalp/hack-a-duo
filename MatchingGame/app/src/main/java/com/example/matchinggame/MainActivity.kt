710deedfe676a5e388191f10972317ad7333e56d
parkerschroeder <ptschroeder@wisc.edu>
2020-07-27 09:47:41 -0500

    Updated duo package

--- MatchingGame/app/src/main/java/com/example/matchinggame/MainActivity.kt
+++ MatchingGame/app/src/main/java/com/example/matchinggame/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.matchinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.microsoft.device.dualscreen.layout.ScreenHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
