c15df0f3c821bd1751e799e3a2086309a017dd03
parkerschroeder <ptschroeder@wisc.edu>
2020-07-27 09:19:40 -0500

    Updated dependencies for surface duo

--- MatchingGame/app/src/main/java/com/example/matchinggame/MainActivity.kt
+++ MatchingGame/app/src/main/java/com/example/matchinggame/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.matchinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.device.dualscreen.layout.ScreenHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
